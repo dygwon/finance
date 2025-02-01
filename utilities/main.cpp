c0e41a27e122ea87a5304cc3f057086a6e4b0726
Daniel Gwon <dygwon@gmail.com>
2025-02-01 15:31:05 -0500

    add cash flow calculator

--- utilities/main.cpp
+++ utilities/main.cpp
@@ -2,32 +2,46 @@
 // main.cpp
 
 // #include "IntRateCalculator.h"
-#include "CompoundIntRateCalculator.h"
+// #include "CompoundIntRateCalculator.h"
+#include "CashFlowCalculator.h"
 
 #include <iostream>
 
 // the main function receives parameters passed to the program
 int main(int argc, const char * argv[])
 {
-    if (argc != 4)
+    if (argc != 2)
     {
-        std::cout << "usage: progName <interest rate> <present value> <num periods>"
-        << std::endl;
+        std::cout << "usage: progName <interest rate>" << std::endl;
         return 1;
     }
 
+    // double rate = atof(argv[1]);
+    // double value = atof(argv[2]);
+    // int num_periods = atoi(argv[3]);
     double rate = atof(argv[1]);
-    double value = atof(argv[2]);
-    int num_periods = atoi(argv[3]);
 
-    CompoundIntRateCalculator cIRCalc(rate);
-    double res = cIRCalc.multiplePeriod(value, num_periods);
-    double contRes = cIRCalc.continuousCompounding(value, num_periods);
-    
-    std::cout << " future value for multiple period compounding is "
-    << res << std::endl;
-    std::cout << " future value for continous compounding is "
-    << contRes << std::endl;
+    // CompoundIntRateCalculator cIRCalc(rate);
+    // double res = cIRCalc.multiplePeriod(value, num_periods);
+    // double contRes = cIRCalc.continuousCompounding(value, num_periods);
+    CashFlowCalculator cfc(rate);
+    do {
+        int period;
+        std::cin >> period;
+        if (period == -1) {
+            break;
+        }
+        double value;
+        std::cin >> value;
+        cfc.addCashPayment(value, period);
+    } while (1);
+
+    // std::cout << " future value for multiple period compounding is "
+    // << res << std::endl;
+    // std::cout << " future value for continous compounding is "
+    // << contRes << std::endl;
+    double result = cfc.presentValue();
+    std::cout << " The present value is " << result << std::endl;
 
     return 0;
 }
